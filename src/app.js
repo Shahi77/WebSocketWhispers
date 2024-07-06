58315103af091edd9dcc229da3921fd47553ec54
Shahi77 <shahiinfinity02@gmail.com>
2024-07-06 12:05:26 +0530

    server setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+module.exports = app;
