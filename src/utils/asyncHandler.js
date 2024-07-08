25bf68271b38b5827b942c01a889cd29a1a3574a
Shahi77 <shahiinfinity02@gmail.com>
2024-07-08 22:45:54 +0530

    adding utilities

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,15 @@
+const asyncHandler = (func) => {
+  return async (req, res, next) => {
+    try {
+      await func(req, res, next);
+    } catch (error) {
+      res.status(error.code || 500).json({
+        success: false,
+        message: error.message,
+        stack: error.stack,
+      });
+    }
+  };
+};
+
+module.exports = asyncHandler;
