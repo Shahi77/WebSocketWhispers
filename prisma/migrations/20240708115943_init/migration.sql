5d75fef275319d3f9b4fb1e219aef2be5bf51ec4
Shahi77 <shahiinfinity02@gmail.com>
2024-07-08 18:59:02 +0530

    adding prisma and DB schema for postgreSQL

---
+++ prisma/migrations/20240708115943_init/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "chats" (
+    "id" SERIAL NOT NULL,
+    "participants" INTEGER[],
+    "messages" INTEGER[],
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "chats_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "messages" (
+    "id" SERIAL NOT NULL,
+    "sender_id" INTEGER NOT NULL,
+    "receiver_id" INTEGER NOT NULL,
+    "message" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_sender_id_fkey" FOREIGN KEY ("sender_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_receiver_id_fkey" FOREIGN KEY ("receiver_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/service/socketServer.js
+++ src/service/socketServer.js
@@ -1,5 +1,6 @@
 const { Server } = require("socket.io");
 const { pub, sub } = require("../service/redis");
+const { prismaClient } = require("./prisma");
 class SocketService {
   #io;
   constructor() {
@@ -11,27 +12,27 @@ class SocketService {
     const io = this.io;
     console.log("Socket listeners init...");
 
-    io.on("connection", (socket) => {
+    io.on("connection", async (socket) => {
       console.log(
         `New user connected to the server with socket_id: ${socket.id}`
       );
 
-      sub.subscribe("MESSAGES");
+      await sub.subscribe("MESSAGES");
       socket.on("send:message", async (message) => {
         await pub.publish("MESSAGES", message);
       });
 
-      sub.on("message", (channel, message) => {
+      sub.on("message", async (channel, message) => {
         if (channel == "MESSAGES") {
           console.log(`${socket.id}: ${message}`);
           io.emit("message", message);
         }
       });
 
-      socket.on("disconnect", (reason) => {
+      socket.on("disconnect", async (reason) => {
         console.log(`Disconnected socket_id: ${socket.id}, reason: ${reason}`);
-        sub.unsubscribe("MESSAGES");
-        sub.removeAllListeners("message");
+        await sub.unsubscribe("MESSAGES");
+        await sub.removeAllListeners("message");
       });
     });
   }
