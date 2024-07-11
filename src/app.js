33c7accaf3c4387682a3b3e32ee3d83c28126494
Shahi77 <shahiinfinity02@gmail.com>
2024-07-11 21:52:59 +0530

    directly navigating to chat page

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const v1Router = require("./routes/version1.routes");
-const verifyToken = require("./middleware/auth.middleware");
+//const v1Router = require("./routes/version1.routes");
+//const verifyToken = require("./middleware/auth.middleware");
 require("dotenv").config();
 const app = express();
 
@@ -19,9 +19,9 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static(path.resolve("./public")));
 app.use(cookieParser());
 
-app.get("/", verifyToken, (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve("./public/chat/index.html"));
 });
 
-app.use("/v1", v1Router);
+//app.use("/v1", v1Router);
 module.exports = app;
