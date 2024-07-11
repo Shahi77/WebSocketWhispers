b20fecda5339b93a5c07739fefa7027d3902ba8a
Shahi77 <shahiinfinity02@gmail.com>
2024-07-11 16:36:21 +0530

    adding logout functionality

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,10 +2,13 @@ const { Router } = require("express");
 const {
   handleUserSignup,
   handleUserLogin,
+  handleUserLogout,
 } = require("../controllers/auth.controller");
+const verifyToken = require("../middleware/auth.middleware");
 
 const authRouter = Router();
 authRouter.post("/signup", handleUserSignup);
 authRouter.post("/login", handleUserLogin);
+authRouter.post("/logout", verifyToken, handleUserLogout);
 
 module.exports = authRouter;
