1aae01429c0081a45be895c90ea0f49adf0e3210
Shahi77 <shahiinfinity02@gmail.com>
2024-07-13 12:59:36 +0530

    fixes

--- src/routes/version1.routes.js
+++ src/routes/version1.routes.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
 const messageRouter = require("./message.routes");
-//const authRouter = require("./auth.routes");
+const authRouter = require("./auth.routes");
 
 const v1Router = Router();
-//v1Router.use("/auth", authRouter);
+v1Router.use("/auth", authRouter);
 v1Router.use("/messages", messageRouter);
 module.exports = v1Router;
