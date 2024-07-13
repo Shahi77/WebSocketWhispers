f990500216754c40dad7efc5d1e1ceeb2f7d0e17
Shahi77 <shahiinfinity02@gmail.com>
2024-07-13 12:58:51 +0530

    removing app.js and keeping index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
-const { server } = require("./service/socket");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const v1Router = require("./routes/version1.routes");
+const { server, app } = require("./service/socketServer");
+const verifyToken = require("./middleware/auth.middleware");
+require("dotenv").config();
+
 const PORT = process.env.PORT || 8000;
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static(path.resolve("./public")));
+app.use(cookieParser());
+
+app.get("/", verifyToken, (req, res) => {
+  res.sendFile(path.resolve("./public/chat/index.html"));
+});
+
+app.use("/v1", v1Router);
 
 const init = async () => {
   server.listen(PORT, () => {
